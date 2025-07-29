ec73cf904f661fbb04c7eb8d4712e12c83ae2da4
olgashablova-del <olga.shablova@gmail.com>
2025-07-29 22:30:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,4 +57,25 @@ async def process_pro(callback_query: types.CallbackQuery):
     )
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    from aiogram.utils.executor import start_webhook
+
+WEBHOOK_HOST = "https://neurobot-d59x.onrender.com"
+WEBHOOK_PATH = "/webhook"
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
+
+if __name__ == "__main__":
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host="0.0.0.0",
+        port=10000
+    )
